3c3367180f50ac5d2e762f77d21a0ee9ae598bca
Shape <shapeintheglass@gmail.com>
2020-10-16 16:55:17 -0700

    More loot container presets

--- src/randomizers/gameplay/level/filters/rules/ApartmentLootRule.java
+++ src/randomizers/gameplay/level/filters/rules/ApartmentLootRule.java
@@ -4,14 +4,33 @@ import org.jdom2.Element;
 
 public class ApartmentLootRule implements Rule {
 
+  private static final String NIGHTSTAND_A = "Containers/Small.Nightstand_A_Day1";
+  private static final String NIGHTSTAND_A2 = "Containers/Small.Nightstand_A2_Day1";
+  private static final String FRIDGE = "Containers/Medium.Refridgerator_A1_Day1";
+  
   @Override
   public boolean trigger(Element e) {
-    return e.getAttributeValue("Name").equals("Containers/Small.Nightstand_A_Day1");
+    return 
+        e.getAttributeValue("Name").equals(NIGHTSTAND_A) 
+        || e.getAttributeValue("Name").equals(NIGHTSTAND_A2)
+        || e.getAttributeValue("Name").equals(FRIDGE);
   }
 
   @Override
   public void apply(Element e) {
-    e.getChild("Properties2").setAttribute("loottable_ContainerLootTable", "RANDOMIZER_MorganApartmentWeaponsNightstand");
+    switch(e.getAttributeValue("Name")) {
+      case NIGHTSTAND_A:
+        e.getChild("Properties2").setAttribute("loottable_ContainerLootTable",
+            "RANDOMIZER_MorganApartmentWeaponsNightstand");
+        break;
+      case NIGHTSTAND_A2:
+        e.getChild("Properties2").setAttribute("loottable_ContainerLootTable",
+            "RANDOMIZER_MorganApartmentWeaponsNightstand2");
+        break;
+      case FRIDGE:
+        e.getChild("Properties2").setAttribute("loottable_ContainerLootTable",
+            "RANDOMIZER_MorganApartmentFridge");
+    }
   }
 
 }
