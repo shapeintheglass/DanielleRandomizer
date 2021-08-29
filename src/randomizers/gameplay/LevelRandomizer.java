5ad8ec52ab47fba8b73dadf1fdb91a2a5fbc3573
June Park <shapeintheglass@gmail.com>
2021-08-29 08:47:57 -0700

    Leave a note that setting TechnoPathDeadForElevator and LiftAtTop to true does not, in fact, unlock the lift

--- src/randomizers/gameplay/LevelRandomizer.java
+++ src/randomizers/gameplay/LevelRandomizer.java
@@ -50,32 +50,44 @@ public class LevelRandomizer extends BaseRandomizer {
 
   private void setupGameTokens() {
     // Use the settings to determine required level game tokens
-    if (settings.getCheatSettings().getUnlockAllScans()) {
+    if (settings.getCheatSettings()
+        .getUnlockAllScans()) {
       gameTokenValues.putAll(LevelConsts.PSYCHOTRONICS_SKIP_CALIBRATION_TOKENS);
     }
-    if (settings.getCheatSettings().getOpenStation()) {
+    if (settings.getCheatSettings()
+        .getOpenStation()) {
       gameTokenValues.putAll(LevelConsts.UNLOCK_QUESTS_GAME_TOKENS);
       gameTokenValues.putAll(LevelConsts.UNLOCK_EXTERIOR_GAME_TOKENS);
       gameTokenValues.putAll(LevelConsts.PSYCHOTRONICS_SKIP_CALIBRATION_TOKENS);
+      //gameTokenValues.putAll(LevelConsts.UNLOCK_LIFT_TOKENS);
     }
-    if (settings.getGameplaySettings().getRandomizeStation()) {
+    if (settings.getGameplaySettings()
+        .getRandomizeStation()) {
       gameTokenValues.putAll(LevelConsts.PSYCHOTRONICS_SKIP_CALIBRATION_TOKENS);
     }
-    if (settings.getExpSettings().getStartSelfDestruct()) {
+    if (settings.getExpSettings()
+        .getStartSelfDestruct()) {
       gameTokenValues.putAll(LevelConsts.START_SELF_DESTRUCT_TOKENS);
     }
-    if (settings.getGameStartSettings().getSkipJovanCutscene()) {
+    if (settings.getGameStartSettings()
+        .getSkipJovanCutscene()) {
       gameTokenValues.putAll(LevelConsts.SKIP_JOVAN_TOKENS);
     }
-    if (settings.getCheatSettings().getUseCustomSpawn()) {
+    if (settings.getCheatSettings()
+        .getUseCustomSpawn()) {
       gameTokenValues.putAll(LevelConsts.ALLOW_GAME_SAVE_TOKENS);
     }
-    if (settings.getGameStartSettings().getStartOutsideApartment()) {
+    if (settings.getGameStartSettings()
+        .getStartOutsideApartment()) {
       gameTokenValues.putAll(LevelConsts.START_OUTSIDE_APARTMENT_TOKENS);
       gameTokenValues.putAll(LevelConsts.ALLOW_GAME_SAVE_TOKENS);
     }
-    if (!settings.getCheatSettings().getGameTokenOverrides().isEmpty()) {
-      String[] tokens = settings.getCheatSettings().getGameTokenOverrides().split(",");
+    if (!settings.getCheatSettings()
+        .getGameTokenOverrides()
+        .isEmpty()) {
+      String[] tokens = settings.getCheatSettings()
+          .getGameTokenOverrides()
+          .split(",");
       for (String token : tokens) {
         if (!token.contains("=")) {
           continue;
@@ -103,7 +115,9 @@ public class LevelRandomizer extends BaseRandomizer {
       while (!toExamine.isEmpty()) {
         String f = toExamine.pop();
         Path inputPath = Paths.get(f);
-        String pathInOutputZip = Paths.get(ZipHelper.DATA_LEVELS + "/" + levelDir).relativize(inputPath).toString();
+        String pathInOutputZip = Paths.get(ZipHelper.DATA_LEVELS + "/" + levelDir)
+            .relativize(inputPath)
+            .toString();
 
         if (zipHelper.isDirectory(f)) {
           if (f.endsWith(TOKENS_FOLDER_NAME)) {
@@ -128,15 +142,13 @@ public class LevelRandomizer extends BaseRandomizer {
               logger.warning("Unable to filter mission file for level " + levelDir);
               e.printStackTrace();
             }
-          } /*else if (swappedLinesMap != null && f.endsWith("moviedata.xml")) {
-            // Filter movie data if applicable
-            try {
-              filterMovieDataFile(f, pathInOutputZip, levelDir);
-            } catch (IOException | JDOMException e) {
-              logger.warning("Unable to filter movie data for level " + levelDir);
-              e.printStackTrace();
-            }
-            } */else {
+          } /*
+             * else if (swappedLinesMap != null && f.endsWith("moviedata.xml")) { // Filter movie
+             * data if applicable try { filterMovieDataFile(f, pathInOutputZip, levelDir); } catch
+             * (IOException | JDOMException e) {
+             * logger.warning("Unable to filter movie data for level " + levelDir);
+             * e.printStackTrace(); } }
+             */else {
             // Copy without filtering
             try {
               zipHelper.copyToLevelPak(f, pathInOutputZip, levelDir);
@@ -153,8 +165,8 @@ public class LevelRandomizer extends BaseRandomizer {
   /**
    * Copies level def into temp directory, while filtering.
    */
-  private void filterMissionFile(String missionFilePath, String pathInZip, String levelDir) throws IOException,
-      JDOMException {
+  private void filterMissionFile(String missionFilePath, String pathInZip, String levelDir)
+      throws IOException, JDOMException {
     Document document = zipHelper.getDocument(missionFilePath);
     Element root = document.getRootElement();
     Element objects = root.getChild("Objects");
@@ -175,15 +187,18 @@ public class LevelRandomizer extends BaseRandomizer {
     }
   }
 
-  private void filterTokensFolder(String gameTokensFolder, String levelDir) throws JDOMException, IOException {
+  private void filterTokensFolder(String gameTokensFolder, String levelDir)
+      throws JDOMException, IOException {
     for (String f : zipHelper.listFiles(gameTokensFolder)) {
-      String pathInOutputZip = Paths.get(ZipHelper.DATA_LEVELS + "/" + levelDir).relativize(Paths.get(f)).toString();
+      String pathInOutputZip = Paths.get(ZipHelper.DATA_LEVELS + "/" + levelDir)
+          .relativize(Paths.get(f))
+          .toString();
       filterTokensFile(f, pathInOutputZip, levelDir);
     }
   }
 
-  private void filterTokensFile(String gameTokensFile, String pathInZip, String levelDir) throws JDOMException,
-      IOException {
+  private void filterTokensFile(String gameTokensFile, String pathInZip, String levelDir)
+      throws JDOMException, IOException {
     Document document = zipHelper.getDocument(gameTokensFile);
     Element root = document.getRootElement();
     List<Element> entities = root.getChildren();
@@ -198,39 +213,22 @@ public class LevelRandomizer extends BaseRandomizer {
   }
 
   /*
-  private void filterMovieDataFile(String movieDataFile, String pathInZip, String levelDir) throws IOException,
-      JDOMException {
-    Document document = zipHelper.getDocument(movieDataFile);
-    Element root = document.getRootElement();
-    List<Element> sequences = root.getChild("Mission").getChild("SequenceData").getChildren();
-  
-    for (Element s : sequences) {
-      List<Element> nodes = s.getChild("Nodes").getChildren();
-      for (Element n : nodes) {
-        List<Element> tracks = n.getChildren();
-        for (Element t : tracks) {
-          String paramType = t.getAttributeValue("paramType");
-          if (paramType == null) {
-            continue;
-          }
-          if (!paramType.equals("Dialog")) {
-            continue;
-          }
-          List<Element> keys = t.getChildren();
-          for (Element k : keys) {
-            String dialogIdHex = k.getAttributeValue("dialogId");
-            String dialogIdDec = new BigInteger(dialogIdHex, 16).toString();
-            if (swappedLinesMap.containsKey(dialogIdDec)) {
-              String newDialogIdHex = new BigInteger(swappedLinesMap.get(dialogIdDec)).toString(16).toUpperCase();
-              k.setAttribute("dialogId", newDialogIdHex);
-            }
-          }
-        }
-  
-      }
-    }
-  
-    zipHelper.copyToLevelPak(document, pathInZip, levelDir);
-  }
-  */
+   * private void filterMovieDataFile(String movieDataFile, String pathInZip, String levelDir)
+   * throws IOException, JDOMException { Document document = zipHelper.getDocument(movieDataFile);
+   * Element root = document.getRootElement(); List<Element> sequences =
+   * root.getChild("Mission").getChild("SequenceData").getChildren();
+   * 
+   * for (Element s : sequences) { List<Element> nodes = s.getChild("Nodes").getChildren(); for
+   * (Element n : nodes) { List<Element> tracks = n.getChildren(); for (Element t : tracks) { String
+   * paramType = t.getAttributeValue("paramType"); if (paramType == null) { continue; } if
+   * (!paramType.equals("Dialog")) { continue; } List<Element> keys = t.getChildren(); for (Element
+   * k : keys) { String dialogIdHex = k.getAttributeValue("dialogId"); String dialogIdDec = new
+   * BigInteger(dialogIdHex, 16).toString(); if (swappedLinesMap.containsKey(dialogIdDec)) { String
+   * newDialogIdHex = new BigInteger(swappedLinesMap.get(dialogIdDec)).toString(16).toUpperCase();
+   * k.setAttribute("dialogId", newDialogIdHex); } } }
+   * 
+   * } }
+   * 
+   * zipHelper.copyToLevelPak(document, pathInZip, levelDir); }
+   */
 }
--- src/utils/LevelConsts.java
+++ src/utils/LevelConsts.java
@@ -11,68 +11,78 @@ public class LevelConsts {
   public static final char DELIMITER = ';';
 
   public static final String PREFIX = "gamesdk/levels/campaign";
-  public static final String[] LEVEL_DIRS = { "endgame", "engineering/cargobay", "engineering/lifesupport",
-      "engineering/powersource", "executive/arboretum", "executive/bridge", "executive/corporateit",
-      "executive/crewfacilities", "research/lobby", "research/prototype", "research/psychotronics",
-      "research/shuttlebay", "research/simulationlabs", "research/zerog_utilitytunnels", "station/exterior" };
+  public static final String[] LEVEL_DIRS = {"endgame", "engineering/cargobay",
+      "engineering/lifesupport", "engineering/powersource", "executive/arboretum",
+      "executive/bridge", "executive/corporateit", "executive/crewfacilities", "research/lobby",
+      "research/prototype", "research/psychotronics", "research/shuttlebay",
+      "research/simulationlabs", "research/zerog_utilitytunnels", "station/exterior"};
 
-  public static final List<String> DO_NOT_TOUCH_ITEM_TAGS = Lists.newArrayList("Data", "MissionItems", "_PROGRESSION",
-      "_PLOT_CRITICAL", "ArkRobots", "ArkLight", "research/simulationlabs;Weapons.Wrench1", "MedKit_Wall_Mounted",
-      "ArkContainer", "Light_Fixtures");
+  public static final List<String> DO_NOT_TOUCH_ITEM_TAGS =
+      Lists.newArrayList("Data", "MissionItems", "_PROGRESSION", "_PLOT_CRITICAL", "ArkRobots",
+          "ArkLight", "research/simulationlabs;Weapons.Wrench1", "MedKit_Wall_Mounted",
+          "ArkContainer", "Light_Fixtures");
 
-  public static final List<String> DO_NOT_OUTPUT_ITEM_TAGS = Lists.newArrayList("Architecture", "Gameplay",
-      "Industrial", "Space", "Light_Fixtures", "Static_ArkLights", "Shotgun_Golden", "DoubleWrench",
-      "Mimic_Placeholder", "OperatorGrenade", "ShotgunPreorderFabPlan", "ArkContainer", "MedKit_Wall_Mounted",
-      "ExplosiveTank_Dynamic");
+  public static final List<String> DO_NOT_OUTPUT_ITEM_TAGS = Lists.newArrayList("Architecture",
+      "Gameplay", "Industrial", "Space", "Light_Fixtures", "Static_ArkLights", "Shotgun_Golden",
+      "DoubleWrench", "Mimic_Placeholder", "OperatorGrenade", "ShotgunPreorderFabPlan",
+      "ArkContainer", "MedKit_Wall_Mounted", "ExplosiveTank_Dynamic");
 
-  public static final List<String> DO_NOT_TOUCH_NPC_TAGS = Lists.newArrayList(
-      "research/psychotronics;ArkNpcSpawner_Weaver6", "research/psychotronics;ArkNpcSpawner_Weaver2",
-      "research/psychotronics;ArkNpcSpawner_Weaver4");
+  public static final List<String> DO_NOT_TOUCH_NPC_TAGS =
+      Lists.newArrayList("research/psychotronics;ArkNpcSpawner_Weaver6",
+          "research/psychotronics;ArkNpcSpawner_Weaver2",
+          "research/psychotronics;ArkNpcSpawner_Weaver4");
 
-  public static final Set<String> CAN_BE_KILLABLE_NPC_TAGS = Sets.newHashSet("research/lobby;ArkNpcSpawner_Technopath1",
-      "research/lobby;ArkNpcSpawner_BasePhantom5", "engineering/lifesupport;ArkNpcSpawner_Technopath1");
+  public static final Set<String> CAN_BE_KILLABLE_NPC_TAGS = Sets.newHashSet(
+      "research/lobby;ArkNpcSpawner_Technopath1", "research/lobby;ArkNpcSpawner_BasePhantom5",
+      "engineering/lifesupport;ArkNpcSpawner_Technopath1");
 
-  public static final List<String> KILLABLE_NPC_ARCHETYPES = Lists.newArrayList("ArkNightmare", "ArkPoltergeist",
-      "Mimics.EliteMimic", "Mimics.Mimic", "Named Phantoms.ArkBeta_ArgentenoPero",
-      "Named Phantoms.ArkBeta_CliveLawrence", "Named Phantoms.ArkBeta_CrispinBoyer",
-      "Named Phantoms.ArkBeta_EnochKouneva", "Named Phantoms.ArkBeta_GarfieldLangley",
-      "Named Phantoms.ArkBeta_HelenCombs", "Named Phantoms.ArkBeta_IvyPark", "Named Phantoms.ArkBeta_JeanFaure",
-      "Named Phantoms.ArkBeta_JorgenThorstein", "Named Phantoms.ArkBeta_KirkRemmer",
-      "Named Phantoms.ArkBeta_LaneCarpenter", "Named Phantoms.ArkBeta_LawrenceBaxter",
-      "Named Phantoms.ArkBeta_MaliahFowles", "Named Phantoms.ArkBeta_NicoleHague",
-      "Named Phantoms.ArkBeta_ReginaSellers", "Named Phantoms.ArkBeta_YuriKimura",
-      "Named Phantoms.Etheric_RandolfHutchinson", "Overseers.Technopath", "Overseers.Telepath", "Overseers.Weaver",
-      "Phantoms.BasePhantom", "Phantoms.EthericDoppelganger", "Phantoms.EthericPhantom", "Phantoms.ThermalPhantom",
-      "Phantoms.VoltaicPhantom");
+  public static final List<String> KILLABLE_NPC_ARCHETYPES =
+      Lists.newArrayList("ArkNightmare", "ArkPoltergeist", "Mimics.EliteMimic", "Mimics.Mimic",
+          "Named Phantoms.ArkBeta_ArgentenoPero", "Named Phantoms.ArkBeta_CliveLawrence",
+          "Named Phantoms.ArkBeta_CrispinBoyer", "Named Phantoms.ArkBeta_EnochKouneva",
+          "Named Phantoms.ArkBeta_GarfieldLangley", "Named Phantoms.ArkBeta_HelenCombs",
+          "Named Phantoms.ArkBeta_IvyPark", "Named Phantoms.ArkBeta_JeanFaure",
+          "Named Phantoms.ArkBeta_JorgenThorstein", "Named Phantoms.ArkBeta_KirkRemmer",
+          "Named Phantoms.ArkBeta_LaneCarpenter", "Named Phantoms.ArkBeta_LawrenceBaxter",
+          "Named Phantoms.ArkBeta_MaliahFowles", "Named Phantoms.ArkBeta_NicoleHague",
+          "Named Phantoms.ArkBeta_ReginaSellers", "Named Phantoms.ArkBeta_YuriKimura",
+          "Named Phantoms.Etheric_RandolfHutchinson", "Overseers.Technopath", "Overseers.Telepath",
+          "Overseers.Weaver", "Phantoms.BasePhantom", "Phantoms.EthericDoppelganger",
+          "Phantoms.EthericPhantom", "Phantoms.ThermalPhantom", "Phantoms.VoltaicPhantom");
 
-  public static final List<String> DO_NOT_OUTPUT_NPC_TAGS = Lists.newArrayList("HumanPlaceholders",
-      "Tentacle_Large_Guard", "Tentacle_Medium_Guard", "Tentacle_Small_Guard", "FakeTechnopath",
-      "Cystoid_IgnorePlayer");
+  public static final List<String> DO_NOT_OUTPUT_NPC_TAGS =
+      Lists.newArrayList("HumanPlaceholders", "Tentacle_Large_Guard", "Tentacle_Medium_Guard",
+          "Tentacle_Small_Guard", "FakeTechnopath", "Cystoid_IgnorePlayer");
 
   // Sets global quest states to unlock important areas.
-  public static final ImmutableMap<String, String> UNLOCK_QUESTS_GAME_TOKENS = new ImmutableMap.Builder<String, String>()
-      .put("CrewFacility.VoiceSampleProcessingPercent", "100")
-      .build();
+  public static final ImmutableMap<String, String> UNLOCK_QUESTS_GAME_TOKENS =
+      new ImmutableMap.Builder<String, String>()
+          .put("CrewFacility.VoiceSampleProcessingPercent", "100")
+          .build();
 
   // Opens all exterior airlocks
-  public static final ImmutableMap<String, String> UNLOCK_EXTERIOR_GAME_TOKENS = new ImmutableMap.Builder<String, String>()
-      .put("AirlockStates.AirlockSealedInternally_ShuttleBay", "false")
-      .put("AirlockStates.AirlockSealedInternally_HardwareLabs", "false")
-      .put("AirlockStates.AirlockSealedInternally_PowerSource", "false")
-      .put("AirlockStates.AirlockSealedInternally_Arboretum", "false")
-      .put("AirlockStates.AirlockSealedInternally_Psychotronics", "false")
-      .build();
-  public static final ImmutableMap<String, String> PSYCHOTRONICS_SKIP_CALIBRATION_TOKENS = ImmutableMap.of(
-      "WorldState.PsychoscopeCalibrationComplete", "true");
-  public static final ImmutableMap<String, String> ENABLE_NIGHTMARE_TOKENS = ImmutableMap.of(
-      "Arboretum.NightmareSpawnedInArboretum", "true");
+  public static final ImmutableMap<String, String> UNLOCK_EXTERIOR_GAME_TOKENS =
+      new ImmutableMap.Builder<String, String>()
+          .put("AirlockStates.AirlockSealedInternally_ShuttleBay", "false")
+          .put("AirlockStates.AirlockSealedInternally_HardwareLabs", "false")
+          .put("AirlockStates.AirlockSealedInternally_PowerSource", "false")
+          .put("AirlockStates.AirlockSealedInternally_Arboretum", "false")
+          .put("AirlockStates.AirlockSealedInternally_Psychotronics", "false")
+          .build();
+  public static final ImmutableMap<String, String> PSYCHOTRONICS_SKIP_CALIBRATION_TOKENS =
+      ImmutableMap.of("WorldState.PsychoscopeCalibrationComplete", "true");
+  public static final ImmutableMap<String, String> ENABLE_NIGHTMARE_TOKENS =
+      ImmutableMap.of("Arboretum.NightmareSpawnedInArboretum", "true");
   public static final String SELF_DESTRUCT_TIMER_TOKEN_NAME = "WorldState.TalosSelfDestructTime";
   public static final ImmutableMap<String, String> START_SELF_DESTRUCT_TOKENS = ImmutableMap.of(
       "CharacterFates.DahlBrainwashed", "true", "AlexAndDahlOutcomes.DahlArrivedInShuttle", "true");
-  public static final ImmutableMap<String, String> SKIP_JOVAN_TOKENS = ImmutableMap.of(
-      "SimLab.Decontamination_SawMimicScene", "true");
-  public static final ImmutableMap<String, String> ALLOW_GAME_SAVE_TOKENS = ImmutableMap.of("WorldState.AllowSaveGame",
-      "true");
-  public static final ImmutableMap<String, String> START_OUTSIDE_APARTMENT_TOKENS = ImmutableMap.of(
-      "LevelTransition.LevelTransitionDestination", "From_Lobby");
+  public static final ImmutableMap<String, String> SKIP_JOVAN_TOKENS =
+      ImmutableMap.of("SimLab.Decontamination_SawMimicScene", "true");
+  public static final ImmutableMap<String, String> ALLOW_GAME_SAVE_TOKENS =
+      ImmutableMap.of("WorldState.AllowSaveGame", "true");
+  public static final ImmutableMap<String, String> START_OUTSIDE_APARTMENT_TOKENS =
+      ImmutableMap.of("LevelTransition.LevelTransitionDestination", "From_Lobby");
+  // Note: These don't work
+  public static final ImmutableMap<String, String> UNLOCK_LIFT_TOKENS =
+      ImmutableMap.of("Lobby.TechnoPathDeadForElevator", "true", "Lobby.LiftAtTop", "true");
 }
